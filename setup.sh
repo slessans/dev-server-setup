5eb478f72c5b92c98fae9d6d7dc52b6e76a0afbc
Scott Lessans <slessans@gmail.com>
2022-07-09 18:29:23 +0000

    fixed home folder portability issue and added conditionals for not reinstalling zoxide,exa when already installed

--- setup.sh
+++ setup.sh
@@ -29,32 +29,44 @@ apt-get install -y --no-install-recommends git zsh fzf bat unzip ncdu
 
 # install exa
 # TODO check for UBUNTU (20.10+) and use apt
-mkdir -p "$SETUP_DIR/exa"
-cd "$SETUP_DIR/exa"
-wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip -O exa.zip
-unzip exa.zip
-mv bin/exa /usr/local/bin/exa
-mv man/exa.1 /usr/local/share/man/man1
-mv completions/exa.bash /usr/share/bash-completion/completions/exa
-mv completions/exa.fish /usr/share/fish/completions/exa.fish
-mv completions/exa.zsh /usr/share/zsh/vendor-completions/_exa
+if ! command -v exa &> /dev/null
+then
+    echo "Installing exa..."
+    mkdir -p "$SETUP_DIR/exa"
+    cd "$SETUP_DIR/exa"
+    wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip -O exa.zip
+    unzip exa.zip
+    mv bin/exa /usr/local/bin/exa
+    mv man/exa.1 /usr/local/share/man/man1
+    mv completions/exa.bash /usr/share/bash-completion/completions/exa
+    mv completions/exa.fish /usr/share/fish/completions/exa.fish
+    mv completions/exa.zsh /usr/share/zsh/vendor-completions/_exa
+else
+    echo "exa already installed, skipping."
+fi
 
-mkdir -p "$SETUP_DIR/zoxide"
-cd "$SETUP_DIR/zoxide"
-wget https://github.com/ajeetdsouza/zoxide/releases/download/v0.8.0/zoxide-v0.8.0-x86_64-unknown-linux-musl.tar.gz -O zoxide.tar.gz
-tar -xvf zoxide.tar.gz
-mv zoxide /usr/local/bin/zoxide
-# we don't worry about completions since the eval "$(zoxide init zsh)" in zshrc handles that 
-# there are some other man pages in there i am ignoring
-mv man/zoxide.1 /usr/local/share/man/man1
+if ! command -v zoxide &> /dev/null
+then
+    echo "Installing zoxide..."
+    mkdir -p "$SETUP_DIR/zoxide"
+    cd "$SETUP_DIR/zoxide"
+    wget https://github.com/ajeetdsouza/zoxide/releases/download/v0.8.0/zoxide-v0.8.0-x86_64-unknown-linux-musl.tar.gz -O zoxide.tar.gz
+    tar -xvf zoxide.tar.gz
+    mv zoxide /usr/local/bin/zoxide
+    # we don't worry about completions since the eval "$(zoxide init zsh)" in zshrc handles that 
+    # there are some other man pages in there i am ignoring
+    mv man/zoxide.1 /usr/local/share/man/man1
+else
+    echo "zoxide already installed, skipping."
+fi
 
 # intall oh-my-zsh
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh > zsh-install.sh
 sudo -u "$SUDO_USER" sh zsh-install.sh --unattended
 
 # install custom theme
-wget https://raw.githubusercontent.com/slessans/oh-my-zsh-sl-agnoster/main/sl-agnoster.zsh-theme -O "/home/$SUDO_USER/.oh-my-zsh/themes/sl-agnoster.zsh-theme"
-cp "$SCRIPT_DIR/.zshrc" "/home/$SUDO_USER/.zshrc"
+sudo -u "$SUDO_USER" wget https://raw.githubusercontent.com/slessans/oh-my-zsh-sl-agnoster/main/sl-agnoster.zsh-theme -O "$HOME/.oh-my-zsh/themes/sl-agnoster.zsh-theme"
+sudo -u "$SUDO_USER" cp "$SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
 
 # change default shell
 sudo chsh -s $(which zsh) "$SUDO_USER"
