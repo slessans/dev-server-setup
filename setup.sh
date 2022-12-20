459fe8af8804dd25b7f8def9fff127ba1078e5ce
Ubuntu <ubuntu@ip-10-0-8-77.ec2.internal>
2022-12-20 23:59:09 +0000

    udpated to work on newer ubuntu versions

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ fi
 SETUP_DIR=$(mktemp -d)
 echo "Created temporary directory $SETUP_DIR"
 cd "$SETUP_DIR"
+chown -R "$SUDO_USER" "$SETUP_DIR"
 
 # ensure certain dirs exist
 mkdir -p /usr/local/share/man/man1
@@ -27,35 +28,47 @@ mkdir -p /usr/share/zsh/vendor-completions
 apt-get update --fix-missing
 apt-get install -y --no-install-recommends git zsh fzf bat unzip ncdu
 
-# install exa
-# TODO check for UBUNTU (20.10+) and use apt
+# install exa, if not exists
 if ! command -v exa &> /dev/null
 then
-    echo "Installing exa..."
-    mkdir -p "$SETUP_DIR/exa"
-    cd "$SETUP_DIR/exa"
-    wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip -O exa.zip
-    unzip exa.zip
-    mv bin/exa /usr/local/bin/exa
-    mv man/exa.1 /usr/local/share/man/man1
-    mv completions/exa.bash /usr/share/bash-completion/completions/exa
-    mv completions/exa.fish /usr/share/fish/completions/exa.fish
-    mv completions/exa.zsh /usr/share/zsh/vendor-completions/_exa
+    # check if apt contains a package for exa
+    if apt-cache show exa &> /dev/null
+    then
+        echo "Installing exa from apt..."
+        apt-get install -y --no-install-recommends exa
+    else
+        echo "Installing exa from prebuilt binaries..."
+        mkdir -p "$SETUP_DIR/exa"
+        cd "$SETUP_DIR/exa"
+        wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip -O exa.zip
+        unzip exa.zip
+        mv bin/exa /usr/local/bin/exa
+        mv man/exa.1 /usr/local/share/man/man1
+        mv completions/exa.bash /usr/share/bash-completion/completions/exa
+        mv completions/exa.fish /usr/share/fish/completions/exa.fish
+        mv completions/exa.zsh /usr/share/zsh/vendor-completions/_exa
+    fi
 else
     echo "exa already installed, skipping."
 fi
 
 if ! command -v zoxide &> /dev/null
 then
-    echo "Installing zoxide..."
-    mkdir -p "$SETUP_DIR/zoxide"
-    cd "$SETUP_DIR/zoxide"
-    wget https://github.com/ajeetdsouza/zoxide/releases/download/v0.8.0/zoxide-v0.8.0-x86_64-unknown-linux-musl.tar.gz -O zoxide.tar.gz
-    tar -xvf zoxide.tar.gz
-    mv zoxide /usr/local/bin/zoxide
-    # we don't worry about completions since the eval "$(zoxide init zsh)" in zshrc handles that 
-    # there are some other man pages in there i am ignoring
-    mv man/zoxide.1 /usr/local/share/man/man1
+    if apt-cache show zoxide &> /dev/null
+    then
+        echo "Installing zoxide from apt..."
+        apt-get install -y --no-install-recommends zoxide
+    else
+        echo "Installing zoxide from prebuilt binaries..."
+        mkdir -p "$SETUP_DIR/zoxide"
+        cd "$SETUP_DIR/zoxide"
+        wget https://github.com/ajeetdsouza/zoxide/releases/download/v0.8.0/zoxide-v0.8.0-x86_64-unknown-linux-musl.tar.gz -O zoxide.tar.gz
+        tar -xvf zoxide.tar.gz
+        mv zoxide /usr/local/bin/zoxide
+        # we don't worry about completions since the eval "$(zoxide init zsh)" in zshrc handles that 
+        # there are some other man pages in there i am ignoring
+        mv man/zoxide.1 /usr/local/share/man/man1
+    fi
 else
     echo "zoxide already installed, skipping."
 fi
@@ -65,8 +78,9 @@ curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh > zsh-
 sudo -u "$SUDO_USER" sh zsh-install.sh --unattended
 
 # install custom theme
-sudo -u "$SUDO_USER" wget https://raw.githubusercontent.com/slessans/oh-my-zsh-sl-agnoster/main/sl-agnoster.zsh-theme -O "$HOME/.oh-my-zsh/themes/sl-agnoster.zsh-theme"
-sudo -u "$SUDO_USER" cp "$SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+sudo -u "$SUDO_USER" wget https://raw.githubusercontent.com/slessans/oh-my-zsh-sl-agnoster/main/sl-agnoster.zsh-theme -O "$USER_HOME/.oh-my-zsh/themes/sl-agnoster.zsh-theme"
+sudo -u "$SUDO_USER" cp "$SCRIPT_DIR/.zshrc" "$USER_HOME/.zshrc"
 
 # change default shell
 sudo chsh -s $(which zsh) "$SUDO_USER"
